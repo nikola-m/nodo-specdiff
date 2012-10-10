e3459869769daa5e6ee1cbfb88b64fe25eebd161
Nikola Mirkov <largeddysimulation@gmail.com>
2012-10-10 09:23:40 +0200

    Subroutine for calulation of Gauss and Gauss-Lobatto nodes.

---
+++ space_discretization.f95
@@ -0,0 +1,41 @@
+      module space_discretization
+!     Discretization for Spectral Difference method.
+      use prec
+      implicit none
+   
+      real(dp), parameter :: pi = 4.*atan(1.0_dp)
+
+      contains
+
+      subroutine gauss_nodes(n,xs)
+!     Solution points - a 1d array of Gauss nodes
+      implicit none
+      integer, intent(in) :: n ! If polynomial is of order (n-1) there are n nodes
+      real(dp),dimension(1:n),intent(out) :: xs ! Gauss nodes
+
+!     Locals
+      integer :: j
+   
+      do j=1,n
+        xs(j) = 0.5*( 1-cos( (2j-1)/2n * pi ) )
+      enddo     
+
+      end subroutine gauss_nodes
+
+      subroutine gl_nodes(n,xs05)
+!     Flux points - a 1d array of Gauss-Lobatto nodes
+      implicit none
+      integer, intent(in) :: n ! If polynomial is of order (n-1) there are n nodes
+      real(dp),dimension(1:n),intent(out) :: xs05 ! Gauss-Lobatto nodes
+
+!     Locals
+      integer :: j
+   
+      do j=1,n
+        xs05(j) = 0.5*( 1-cos( j/n * pi ) )
+      enddo     
+
+      end subroutine gl_nodes
+      
+
+      end module space_discretization
